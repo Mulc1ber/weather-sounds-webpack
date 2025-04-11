e510f460b480de8cb3496fca1ce0bf66ef8b214a
Mulc1ber <mike@m.ru>
2025-04-11 15:56:14 +0300

    feat: Реализовал основной функционал приложения

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -8,8 +10,41 @@ module.exports = {
     filename: "[name].[contenthash].js",
     clean: true,
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/images"),
+          to: path.resolve(__dirname, "dist/images"),
+        },
+        {
+          from: path.resolve(__dirname, "public/icons"),
+          to: path.resolve(__dirname, "dist/icons"),
+        },
+        {
+          from: path.resolve(__dirname, "public/sounds"),
+          to: path.resolve(__dirname, "dist/sounds"),
+        },
+        {
+          from: path.resolve(__dirname, "public/favicon.png"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
