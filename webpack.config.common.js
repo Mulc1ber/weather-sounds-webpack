dc52851812f3053fded95a25e5c4771c46a2a2d2
Mulc1ber <mike@m.ru>
2025-04-11 16:09:36 +0300

    build(webpack): добавил mini-css-extract-plugin для выноса стилей в отдельный файл

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -34,12 +35,13 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
