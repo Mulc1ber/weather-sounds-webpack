aaa349557e03074e38b272d3de8f0f5234009eaa
Mulc1ber <mike@m.ru>
2025-04-11 15:29:11 +0300

    fix(webpack): исправил поле entry в webpack конфиге

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: "./index.js",
+  entry: "./app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
