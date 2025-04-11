ae94f2f912a8931365bfb90c05c4718687a060a4
Mulc1ber <mike@m.ru>
2025-04-11 15:09:32 +0300

    init webpack

---
+++ webpack.config.common.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  plugins: [],
+  module: {
+    rules: [],
+  },
+};
